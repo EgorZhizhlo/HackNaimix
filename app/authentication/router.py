938681a105382ab134b62c42ba18b05c30f5cfcb
Egor Zhizhlo <zhizhloegor_r@mail.ru>
2024-11-15 14:33:40 +0300

    Create base form of project

--- app/authentication/router.py
+++ app/authentication/router.py
@@ -6,16 +6,16 @@ auth_router = APIRouter(prefix='/auth', tags=['Авторизация'])
 
 
 @auth_router.post('/reg', tags=['Регистрация'])
-async def auth_home(
+async def auth_registration(
         response: Response,
         user: RegistrationModel = Form(...)
         ):
     return user.dict()
 
 
-@auth_router.post('/reg', tags=['Регистрация'])
-async def auth_home(
+@auth_router.post('/log', tags=['Вход'])
+async def auth_login(
         response: Response,
-        user: RegistrationModel = Form(...)
+        user: LoginModel = Form(...)
         ):
     return user.dict()
